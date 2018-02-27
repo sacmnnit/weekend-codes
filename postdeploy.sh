fa03d9067b3c032746ad6d941a74ee11c48c6fb5
Deepak Bharti <deepakbharti823@gmail.com>
2018-02-27 21:51:51 +0530

    allowing execution permission to postdeploy.sh

--- postdeploy.sh
+++ postdeploy.sh
@@ -1,1 +1,1 @@
-#echo "from django.contrib.auth.models import User; User.objects.create_superuser('deepak', 'admin@example.com', 'cooladmins@3')" | python manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('deepak', 'admin@example.com', 'cooladmins@3')" | python manage.py shell
