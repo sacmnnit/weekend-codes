25aae41ba10970a043eebe9617297823c0bdad27
Deepak Bharti <deepakbharti823@gmail.com>
2018-03-01 00:39:19 +0530

    synch with type

---
+++ WocLanding/static/WocLanding/js/typing_animation.js
@@ -0,0 +1,75 @@
+
+
+$(document).ready(function(){
+
+$('.send_message').typeIt({ 
+  // breakLines:false, 
+  speed: 100,
+  // deleteDelay: 750, 
+  autoStart: false,
+  // loop:true,
+  // loopDelay:1500 
+});
+
+$('.type_slider').typeIt({  
+  speed: 100,
+  autoStart: false,
+});
+
+$('.type_why_woc').typeIt({  
+  speed: 100,
+  autoStart: false,
+});
+
+$('.type_woc10').typeIt({  
+  speed: 300,
+  loop:true,
+  autoStart: false,
+});
+
+$('.type_devjam_codestart').typeIt({  
+  speed: 100,
+  autoStart: false,
+});
+
+
+$('.type_event_location').typeIt({  
+  speed: 100,
+  // startDelay: 100,
+  autoStart: false,
+});
+
+$('.type_event_time').typeIt({  
+  speed: 100,
+  startDelay: 2500,
+  autoStart: false,
+});
+
+$('.type_event_speakers').typeIt({  
+  speed: 100,
+  startDelay: 4500,
+  autoStart: false,
+});
+
+$('.type_event_registration').typeIt({  
+  speed: 100,
+  startDelay: 6500,
+  autoStart: false,
+});
+
+// $('.type_devjam').typeIt({  
+//   speed: 100,
+//   loop:true,
+//   autoStart: false,
+// });
+// </script>
+
+// <script type="text/javascript">
+// $('.type_codestart').typeIt({  
+//   speed: 100,
+//   loop:true,
+//   autoStart: false,
+// });
+
+});
+
